3205dc00f186167d883a8910390be09f07f3df31
jongeuni <lje0818ovo@naver.com>
2024-12-07 22:07:28 +0900

    [FIX] Swagger 개발 서버 https로 보내도록 변경

--- src/main/java/com/mondol/mhmh/config/SwaggerConfig.java
+++ src/main/java/com/mondol/mhmh/config/SwaggerConfig.java
@@ -1,11 +1,18 @@
 package com.mondol.mhmh.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://mhmh-backend.fly.dev", description = "운영 서버"),
+                @Server(url = "http://localhost:8080", description = "로컬 서버")
+        })
 @Configuration
 public class SwaggerConfig {
     @Bean
