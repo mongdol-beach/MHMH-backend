12fdf5d162438cc46837aa567344dd51b7a6e870
jongeuni <lje0818ovo@naver.com>
2024-12-07 11:29:50 +0900

    [INIT]

---
+++ src/main/java/com/mondol/mhmh/MhmhApplication.java
@@ -0,0 +1,13 @@
+package com.mondol.mhmh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhmhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhmhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mondol/mhmh/MhmhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mondol.mhmh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhmhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
