ab03e80c54b09ab8cd15d9f585488fd90ba9562f
Dong-Young Kim <31337.persona@gmail.com>
2024-12-08 05:42:25 +0900

    feat: 밸런스 게임 질문 조회 로직 구현

--- src/main/java/com/mondol/mhmh/balanceGame/BalanceGameController.java
+++ src/main/java/com/mondol/mhmh/balanceGame/BalanceGameController.java
@@ -1,5 +1,7 @@
 package com.mondol.mhmh.balanceGame;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mondol.mhmh.balanceGame.dto.BalanceQuestionReadDto;
 import com.mondol.mhmh.balanceGame.rqrs.BalanceAnswerRq;
 import com.mondol.mhmh.balanceGame.rqrs.BalanceAnswerRs;
 import com.mondol.mhmh.balanceGame.rqrs.BalanceQuestionListRs;
@@ -15,7 +18,6 @@ import com.mondol.mhmh.balanceGame.service.BalanceGameService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-
 
 @Tag(name = "balance-game", description = "밸런스 게임 API")
 @RestController
@@ -28,7 +30,8 @@ public class BalanceGameController {
     @Operation(summary = "밸런스 게임 질문 조회 (5개)")
     @GetMapping("/questions")
     public BalanceQuestionListRs readBalanceQuestionList() {
-        return BalanceQuestionListRs.of(this.gameService.readFiveRandomBalanceQuestionList());
+        List<BalanceQuestionReadDto> questions = this.gameService.readFiveRandomBalanceQuestionList();
+        return BalanceQuestionListRs.of(questions);
     }
 
     @Operation(summary = "밸런스 게임 질문 응답")
--- src/main/java/com/mondol/mhmh/balanceGame/service/BalanceGameService.java
+++ src/main/java/com/mondol/mhmh/balanceGame/service/BalanceGameService.java
@@ -1,78 +1,32 @@
 package com.mondol.mhmh.balanceGame.service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
-import com.mondol.mhmh.balanceGame.rqrs.BalanceQuestion;
+import com.mondol.mhmh.balanceGame.dto.BalanceQuestionReadDto;
+import com.mondol.mhmh.balanceGame.repository.BalanceQuestionRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class BalanceGameService {
 
-    public List<BalanceQuestion> readFiveRandomBalanceQuestionList() {
-        List<BalanceQuestion> allQuestions = readBalanceQuestionList();
-        List<BalanceQuestion> randomQuestions = new ArrayList<>();
+    private final BalanceQuestionRepository balanceQuestionRepository;
 
-        Random random = new Random();
-        Set<Integer> selectedIndices = new HashSet<>();
-
-        while (selectedIndices.size() < 5) {
-            int randomIndex = random.nextInt(allQuestions.size());
-            if (selectedIndices.add(randomIndex)) {
-                randomQuestions.add(allQuestions.get(randomIndex));
-            }
-        }
-
-        return randomQuestions;
+    public List<BalanceQuestionReadDto> readFiveRandomBalanceQuestionList() {
+        List<BalanceQuestionReadDto> allQuestions = readAllBalanceQuestion();
+        // REVIEW: 만약 질문이 5개 미만이라면 예외를 발생시켜야 할까?
+        Collections.shuffle(allQuestions);
+        return allQuestions.subList(0, 5);
     }
 
-    public List<BalanceQuestion> readBalanceQuestionList() {
-        List<BalanceQuestion> list = new ArrayList<>();
-        list.add(new BalanceQuestion(1, "설명", "강동원", "유병재"));
-        list.add(new BalanceQuestion(2, "설명", "평생 두통", "평생 치통"));
-        list.add(new BalanceQuestion(3, "설명", "겨울에 에어컨", "여름에 히터"));
-        list.add(new BalanceQuestion(4, "설명", "언제 죽을지 알기", "어떻게 죽을지 알기"));
-        list.add(new BalanceQuestion(5, "설명", "고양이 키우기", "강아지 키우기"));
-        list.add(new BalanceQuestion(6, "설명", "휴대폰 배터리 1%", "인터넷 안됨"));
-        list.add(new BalanceQuestion(7, "설명", "과거로 돌아가기", "미래로 가보기"));
-        list.add(new BalanceQuestion(8, "설명", "똥 맛 카레 먹기", "카레맛 똥 먹기"));
-        list.add(new BalanceQuestion(9, "설명", "잠수 이별 당하기", "환승 이별 당하기"));
-        list.add(new BalanceQuestion(10, "설명", "나를 좋아하는 사람", "내가 좋아하는 사람"));
-        list.add(new BalanceQuestion(11, "설명", "인스타 평생 금지", "유튜브 평생 금지"));
-        list.add(new BalanceQuestion(12, "설명", "연인과 하루에 100번 연락하기", "연인과 한 달에 한 번 연락하기"));
-        list.add(new BalanceQuestion(13, "설명", "(싫어하는 사람한테)단톡에서 고백받기", "(싫어하는 사람한테)사람많은 길거리에서 고백받기"));
-        list.add(new BalanceQuestion(14, "설명", "일주일 내내 집에 있기", "일주일 내내 외출하기"));
-        list.add(new BalanceQuestion(15, "설명", "키 130cm", "키 250cm"));
-        list.add(new BalanceQuestion(16, "설명", "평생 여드름 피부", "평생 탈모"));
-        list.add(new BalanceQuestion(17, "설명", "365일 폭염", "365일 장마"));
-        list.add(new BalanceQuestion(18, "설명", "100% 확률로 1억 받기", "50% 확률로 5억 받기"));
-        list.add(new BalanceQuestion(19, "설명", "(실패했을 때)위로와 격려 받기", "(실패했을 때)냉정한 피드백 받기"));
-        list.add(new BalanceQuestion(20, "설명", "전화", "카톡 or 문자"));
-        list.add(new BalanceQuestion(21, "설명", "돈 많이 주는 하기 싫은 일", "돈 적게 주는 하고 싶은 일"));
-        list.add(new BalanceQuestion(22, "설명", "서서 30분 출퇴근", "앉아서 1시간 출퇴근"));
-        list.add(new BalanceQuestion(23, "설명", "애인과 1시간 싸우기", "상사한테 1시간 혼나기"));
-        list.add(new BalanceQuestion(24, "설명", "일 잘하는데 성격 안좋음", "일 잘하는데 성격 좋음"));
-        list.add(new BalanceQuestion(25, "설명", "지나치게 간섭하는 사수", "아무것도 안 알려주는 사수"));
-        list.add(new BalanceQuestion(26, "설명", "(사녀 연애할 때)공개", "(사녀 연애할 때)비공개"));
-        list.add(new BalanceQuestion(27, "설명", "평생 쌀만 먹기", "평생 빵만 먹기"));
-        list.add(new BalanceQuestion(28, "설명", "음악에 재능 갖기", "미술에 재능 갖기"));
-        list.add(new BalanceQuestion(29, "설명", "머리카락 못 자르기", "손톱 못 자르기"));
-        list.add(new BalanceQuestion(30, "설명", "원하는 얼굴로 살기", "원하는 몸매로 살기"));
-        list.add(new BalanceQuestion(31, "설명", "평생 긴팔 입기", "평생 반팔 입기"));
-        list.add(new BalanceQuestion(32, "설명", "존잘 노잼", "존못 꿀잼"));
-        list.add(new BalanceQuestion(33, "설명", "딸기 라면", "초콜렛 카레"));
-        list.add(new BalanceQuestion(34, "설명", "상사한테 야동 링크 잘못 보내기", "상사한테 뒷담화 카톡 잘못 보내기"));
-        list.add(new BalanceQuestion(35, "설명", "눈 3개", "눈 1개"));
-        list.add(new BalanceQuestion(36, "설명", "세상 사람들이 나한테만 말걸기", "세상 사람들이 나한테만 말 안걸기"));
-        list.add(new BalanceQuestion(37, "설명", "미지근하고 탄산 있는 콜라", "시원한데 탄산 없는 콜라"));
-        list.add(new BalanceQuestion(38, "설명", "이성 친구와 1박2일 여행가는 애인", "전 애인과 밤에 단둘이 술 마시는 애인"));
-        list.add(new BalanceQuestion(39, "설명", "파인애플 피자", "민트 초코"));
-        list.add(new BalanceQuestion(40, "설명", "싸운 후 다 이야기하고 다니는 애인", "싸운 후 전혀 타격감 없는 애인"));
-        return list;
+    public List<BalanceQuestionReadDto> readAllBalanceQuestion() {
+        return this.balanceQuestionRepository.findAll().stream()
+                .map(BalanceQuestionReadDto::from)
+                .toList();
     }
 
 }
