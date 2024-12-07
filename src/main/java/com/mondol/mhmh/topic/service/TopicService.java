bb023e272f12d9367edf1d2393bdc4d93ce1dc86
jongeuni <lje0818ovo@naver.com>
2024-12-07 17:55:57 +0900

    [#5] 토픽 조회 시 tip 추가

--- src/main/java/com/mondol/mhmh/topic/service/TopicService.java
+++ src/main/java/com/mondol/mhmh/topic/service/TopicService.java
@@ -11,11 +11,11 @@ import java.util.List;
 public class TopicService {
     public TopicReadListRs readTopicList(int count) {
         List<TopicReadItemRs> list = new ArrayList<>();
-        list.add(new TopicReadItemRs(1, "기분 어때요?", false));
-        list.add(new TopicReadItemRs(23, "날씨 어때요?", false));
-        list.add(new TopicReadItemRs(231, "책 어때요?", false));
-        list.add(new TopicReadItemRs(21, "공부 어때요?", false));
-        list.add(new TopicReadItemRs(2, "인사해보세요", false));
+        list.add(new TopicReadItemRs(1, "기분 어때요?", false, "tip입니다."));
+        list.add(new TopicReadItemRs(23, "날씨 어때요?", false, "1. 이렇게 하기 \n - 이렇게요 \n 2. 이렇게 하면 됩니다"));
+        list.add(new TopicReadItemRs(231, "책 어때요?", false, "tip"));
+        list.add(new TopicReadItemRs(21, "공부 어때요?", false, "tip"));
+        list.add(new TopicReadItemRs(2, "인사해보세요", false, "tip"));
         return TopicReadListRs.of(list);
     }
 }
