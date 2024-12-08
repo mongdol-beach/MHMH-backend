ce951accf33156f3b4c99c093265876845e08765
jongeuni <lje0818ovo@naver.com>
2024-12-08 10:44:44 +0900

    [#13] 상황 init sql 작성

--- src/main/sql/init.sql
+++ src/main/sql/init.sql
@@ -1,1 +1,9 @@
--- topic
+-- situation insert
+INSERT INTO situation values ('DATE', '소개팅', '5881F3', 'FFFFFF');
+INSERT INTO situation values ('COMPANY', '회사', '5881F3', 'FFFFFF');
+INSERT INTO situation values ('COFFEE', '커피챗', '5881F3', 'FFFFFF');
+INSERT INTO situation values ('SMALL', '스몰토크', '5881F3', 'FFFFFF');
+INSERT INTO situation values ('TOGETHER', '단체 모임', '5881F3', 'FFFFFF');
+INSERT INTO situation values ('COUPLE', '커플', '5881F3', 'FFFFFF');
+
+-- topic insert
