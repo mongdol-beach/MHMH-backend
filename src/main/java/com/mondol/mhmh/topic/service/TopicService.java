ad3911572755ab6e61112bffa36ef2e5414db02f
jongeuni <lje0818ovo@naver.com>
2024-12-08 10:19:59 +0900

    [#5] RS 변경

--- src/main/java/com/mondol/mhmh/topic/service/TopicService.java
+++ src/main/java/com/mondol/mhmh/topic/service/TopicService.java
@@ -1,8 +1,6 @@
 package com.mondol.mhmh.topic.service;
 
-import com.mondol.mhmh.topic.rqrs.TopicReadItemRs;
-import com.mondol.mhmh.topic.rqrs.TopicReadListRs;
-import com.mondol.mhmh.topic.rqrs.TopicTipRs;
+import com.mondol.mhmh.topic.rqrs.*;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,11 +14,24 @@ public class TopicService {
         List<TopicTipRs> rs = new ArrayList<>();
         rs.add(new TopicTipRs("소제목 1", Arrays.asList("설명1", "설명2", "설명3")));
         rs.add(new TopicTipRs("소제목 2", Arrays.asList("설명1", "설명2")));
-        list.add(new TopicReadItemRs(1, "기분 어때요?", false, rs));
-        list.add(new TopicReadItemRs(23, "날씨 어때요?", false, rs));
-        list.add(new TopicReadItemRs(231, "책 어때요?", false, rs));
-        list.add(new TopicReadItemRs(21, "공부 어때요?", false, rs));
-        list.add(new TopicReadItemRs(2, "인사해보세요", false, rs));
+        list.add(new TopicReadItemRs(1, "기분 어때요?", rs));
+        list.add(new TopicReadItemRs(23, "날씨 어때요?", rs));
+        list.add(new TopicReadItemRs(231, "책 어때요?",  rs));
+        list.add(new TopicReadItemRs(21, "공부 어때요?", rs));
+        list.add(new TopicReadItemRs(2, "인사해보세요", rs));
         return TopicReadListRs.of(list);
+    }
+
+    public AllTopicReadListRs readALlTopic(int count) {
+        List<AllTopicReadItemRs> list = new ArrayList<>();
+        List<TopicTipRs> rs = new ArrayList<>();
+        rs.add(new TopicTipRs("소제목 1", Arrays.asList("설명1", "설명2", "설명3")));
+        rs.add(new TopicTipRs("소제목 2", Arrays.asList("설명1", "설명2")));
+        list.add(new AllTopicReadItemRs(1, "기분 어때요?", rs));
+        list.add(new AllTopicReadItemRs(23, "날씨 어때요?",  rs));
+        list.add(new AllTopicReadItemRs(231, "책 어때요?",  rs));
+        list.add(new AllTopicReadItemRs(21, "공부 어때요?", rs));
+        list.add(new AllTopicReadItemRs(2, "인사해보세요",  rs));
+        return AllTopicReadListRs.of("DATE","소개팅", list);
     }
 }
