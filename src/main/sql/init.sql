06fd721bb3468177684278098ff03cf7511fc2cf
jongeuni <lje0818ovo@naver.com>
2024-12-08 12:22:54 +0900

    [#14] 토픽 sql 작성

--- src/main/sql/init.sql
+++ src/main/sql/init.sql
@@ -7,3 +7,17 @@ INSERT INTO situation values ('TOGETHER', '단체 모임', '5881F3', 'FFFFFF');
 INSERT INTO situation values ('COUPLE', '커플', '5881F3', 'FFFFFF');
 
 -- topic insert
+START TRANSACTION;
+
+INSERT INTO topic VALUES (1, '질문입니다.', 'DATE');
+
+INSERT INTO topic_tip (topic_tip_id, title, topic_id) VALUES (1, '소제목 1입니다.', 1);
+INSERT INTO topic_tip_contents (c_topic_tip_id, contents)
+VALUES (1, 'Tip Content 1'),
+       (1, 'Tip Content 2');
+INSERT INTO topic_tip (topic_tip_id, title, topic_id) VALUES (2, '소제목 2입니다.', 1);
+INSERT INTO topic_tip_contents (c_topic_tip_id, contents)
+VALUES (2, 'Tip Content 1'),
+       (2, 'Tip Content 2');
+
+END;
