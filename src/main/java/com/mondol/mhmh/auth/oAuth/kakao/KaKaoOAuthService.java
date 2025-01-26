93117302ee290ac88dcb893b8d18fc8cb6d168fb
jongeuni <lje0818ovo@naver.com>
2025-01-27 00:46:26 +0900

    #29 redirect uri 변경

--- src/main/java/com/mondol/mhmh/auth/oAuth/kakao/KaKaoOAuthService.java
+++ src/main/java/com/mondol/mhmh/auth/oAuth/kakao/KaKaoOAuthService.java
@@ -63,7 +63,7 @@ public class KaKaoOAuthService {
             String accessToken = jwtUtil.generateAccessToken(id, userInfo.getEmail());
             String refreshToken = jwtUtil.generateRefreshToken(id);
 
-            URI redirectUri = URI.create("http://localhost:5173/login/kakao");
+            URI redirectUri = URI.create("http://localhost:5173");
 
             // 헤더 설정
             HttpHeaders headers2 = new HttpHeaders();
