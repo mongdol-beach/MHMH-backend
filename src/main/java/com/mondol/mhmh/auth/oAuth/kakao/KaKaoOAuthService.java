7587214bce92e4db6c7e35e0a0ca9fa5feda69ca
jongeuni <lje0818ovo@naver.com>
2025-01-31 01:40:11 +0900

    헤더 origin으로 가져오도록

--- src/main/java/com/mondol/mhmh/auth/oAuth/kakao/KaKaoOAuthService.java
+++ src/main/java/com/mondol/mhmh/auth/oAuth/kakao/KaKaoOAuthService.java
@@ -43,7 +43,8 @@ public class KaKaoOAuthService {
             RestTemplate restTemplate = new RestTemplate();
 //            String host = request.getRequestURL().toString();
 
-            String host = request.getServerName() + ":" + request.getServerPort();
+//            String host = request.getServerName() + ":" + request.getServerPort();
+            String host = request.getHeader("ORIGIN");
             log.debug(host+ " :request host");
             if (host.contains("localhost:5173")) {
                 // 로컬 환경
